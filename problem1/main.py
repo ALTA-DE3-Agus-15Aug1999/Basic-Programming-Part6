c6748b2a45f2beb5266fc131d031687689ae981a
agusfn15 <nagusfernando@gmail.com>
2024-05-16 21:57:22 +0700

    solve problem 1

--- problem1/main.py
+++ problem1/main.py
@@ -1,5 +1,13 @@
 def compare(a, b):
     pattern = ""
+    min_length = min(len(a), len(b))
+
+    for i in range(min_length):
+        if a [i] == b [i]:
+            pattern += a[i]
+        else:
+            break
+        
     return pattern
 
 if __name__ == '__main__':
