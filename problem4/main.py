3d301db0cd3cadd6bcf289d95d40317cd6b0609b
agusfn15 <nagusfernando@gmail.com>
2024-05-17 21:42:26 +0700

    solve problem 4

--- problem4/main.py
+++ problem4/main.py
@@ -1,5 +1,16 @@
 def find_max_sum_sub_array(k, arr):
-    return 0
+    max_sum = 0
+    window_sum = 0
+    window_start = 0
+
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
+
+        if window_end >= k - 1:
+            max_sum = max(max_sum, window_sum)
+            window_sum -= arr[window_start]
+            window_start += 1
+    return max_sum
 
 if __name__ == '__main__':
     print(find_max_sum_sub_array(3, [2, 1, 5, 1, 3, 2])) # 9
